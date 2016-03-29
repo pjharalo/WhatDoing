88cec4d425ba2fec0e926496f318a70386d31c1b
Peter Haralovich <peterharalovich@Peters-MacBook-Pro.local>
2016-03-29 15:06:02 -0400

    Setup XCode pods

---
+++ iOS/WhatDoing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WhatDoing
+//
+//  Created by Peter Haralovich on 3/29/16.
+//  Copyright © 2016 Peter Haralovich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
